29b614057478a5615f0253a1aea4df0184968224
rachekalmir <rachekalmir@users.noreply.github.com>
2017-06-24 20:44:00 +0200

    Fixed requirements for travisci

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 Python JOLT Implementation
 """
 from setuptools import setup
-from pip.req import parse_requirements
 
-from pyjolt import __version__
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
-install_requires = [str(ir.req) for ir in parse_requirements('requirements.txt')]
-test_requires = [str(ir.req) for ir in parse_requirements('test-requirements.txt')]
+with open('test-requirements.txt') as f:
+    tests_requires = f.read().splitlines()
 
 setup(
     name='pyjolt',
-    version=__version__,
+    version='0.0.1',
     description='Python JOLT Implementation',
     long_description=__doc__,
     author='rachekalmir',
@@ -20,5 +20,5 @@ setup(
     license='Apache 2.0',
     packages=['pyjolt'],
     install_requires=install_requires,
-    tests_requires=test_requires,
+    tests_requires=tests_requires,
 )
